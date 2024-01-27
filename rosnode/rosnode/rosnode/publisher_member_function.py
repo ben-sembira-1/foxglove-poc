78688fa9303bae5332d3085c2a3118fe811e2f46
Ben <ben.sembira.1@gmail.com>
2024-01-27 11:02:11 +0200

    create simple ros2 pubsub node

---
+++ rosnode/rosnode/rosnode/publisher_member_function.py
@@ -0,0 +1,63 @@
+# Copyright 2016 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import rclpy
+from rclpy.node import Node
+
+from std_msgs.msg import String
+
+
+class MinimalPubSub(Node):
+
+    def __init__(self):
+        super().__init__('minimal_pubsub')
+        self.last_message = "--"
+        timer_period = 0.5  # seconds
+        self.i = 0
+        self.subscription = self.create_subscription(
+            String,
+            '/update',
+            self.update_last_message,
+            10
+        )
+        self.publisher_ = self.create_publisher(String, '/hey', 10)
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+
+    def update_last_message(self, new_message: str):
+        self.last_message = new_message
+
+    def timer_callback(self):
+        msg = String()
+        msg.data = f'Hello World: {self.i}. Last message is: {self.last_message}'
+        self.publisher_.publish(msg)
+        self.get_logger().info('Publishing: "%s"' % msg.data)
+        self.i += 1
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    minimal_publisher = MinimalPubSub()
+
+    rclpy.spin(minimal_publisher)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    minimal_publisher.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
